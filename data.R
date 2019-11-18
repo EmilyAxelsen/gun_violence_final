6539a809f679fb3ac218396daa47c18a56ed6428
Emily Axelsen <emilyaxelsen@college.harvard.edu>
2019-11-18 11:39:00 -0500

    inner join work

--- data.R
+++ data.R
@@ -90,13 +90,16 @@ saveRDS(final_data, "final_data.rds")
 
 state <- read_xlsx("data/correlatesofstatepolicyprojectv2_1 NEED.xlsx")
 
+state %>%
+  unlist(year) %>%
+  as.character(year)
 
 new_state <- state %>%
   filter(year %in% c(2013, 2014, 2015, 2016, 2017, 2018)) %>%
   dplyr::select(state, year, guncontrol_assaultweapon_ban, w_guncontrol_waitingperiod, 
                 guncontrol_stand_your_ground, w_guncontrol_registration_requir, 
                 guncontrol_opencarry) 
-  #as.numeric(unlist(year))
+  
 
 
 saveRDS(new_state, "state_policy.rds")
